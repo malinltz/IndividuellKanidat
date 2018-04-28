98349fd4d17078eb434a984150aee64db858ea2c
Simon <simonistgren96@gmail.com>
2018-04-28 11:20:30 +0200

    Uppdate

--- Kandidat_individuella.m
+++ Kandidat_individuella.m
@@ -33,15 +33,13 @@ kundlista = [kundlista, A',A',A',A']; %Kundens status motsvarar,
 klockan = 0; % klockan går mellan 0 och 57600 sekunder (16 timmar).
 riktning_x = zeros(10,4); % Riktningen som taxin ska färdas i x-led.
 riktning_y = zeros(10,4); % Riktningen som taxin ska färdas i y-led.
-storlek = 0;
+
 %Lägg till en till for-loop för att köra heuristiken flera gånger.
 for i = 0:tid
     for j = 1:rows
-        if (klockan == kundlista(j,6)) % Kund ringer in
-            kundlista(j,8) = 1;% Taxibil skickas till en kund
-            for k = 1:10
-                if (Taxibilar(k,1) == 0  && kundlista(j,11) == 0) % Tilldelar en taxi ett jobb om taxin är ledig
-                    
+        if (kundlista(j,8) == -1)
+             for k = 1:10
+                if (Taxibilar(k,1) == 0) % Tilldelar en taxi ett jobb om taxin är ledig
                     Taxibilar(k,4) = Taxibilar(k,4) + 1; % Counter, för hur många körningar varje taxibil tar på sig.
                     disp(['Taxibil nr: ',num2str(k),' betjänar kund nr: ', num2str(j)])
                     Taxibilar(k,1) = 1; % Sätter taxibilens status till 2.
@@ -56,15 +54,38 @@ for i = 0:tid
                     riktning_x(k,4) = riktning_x(k,3)/abs(riktning_x(k,3));
                     riktning_y(k,3) = (kundlista(j,4) - kundlista(j,2));
                     riktning_y(k,4) = riktning_y(k,3)/abs(riktning_y(k,3));
-                    
+                    kundlista(j,8) = 1;% Taxibil skickas till en kund
                     kundlista(j,11) = k;
                     break % Avbryter loopen när en taxi tilldelas en kund.
                 end
+             end
+        end
+        if (klockan == kundlista(j,6)) % Kund ringer in
+            for k = 1:10
+                if (Taxibilar(k,1) == 0) % Tilldelar en taxi ett jobb om taxin är ledig
+                    Taxibilar(k,4) = Taxibilar(k,4) + 1; % Counter, för hur många körningar varje taxibil tar på sig.
+                    disp(['Taxibil nr: ',num2str(k),' betjänar kund nr: ', num2str(j)])
+                    Taxibilar(k,1) = 1; % Sätter taxibilens status till 2.
+                    Taxibilar(k,5) = 0; % Återställer tiden för kunden att ta sig in/ut ur bilen.
+                    % Beräknar riktningen taxin ska färdas för att hämta upp en kund.
+                    riktning_x(k,1) = (kundlista(j,1) - Taxibilar(k,2));
+                    riktning_x(k,2) = riktning_x(k,1)/abs(riktning_x(k,1));
+                    riktning_y(k,1) = (kundlista(j,2) - Taxibilar(k,3));
+                    riktning_y(k,2) = riktning_y(k,1)/abs(riktning_y(k,1));
+                    % Beräknar riktningen taxin ska färdas för att lämna av en kund.
+                    riktning_x(k,3) = (kundlista(j,3) - kundlista(j,1));
+                    riktning_x(k,4) = riktning_x(k,3)/abs(riktning_x(k,3));
+                    riktning_y(k,3) = (kundlista(j,4) - kundlista(j,2));
+                    riktning_y(k,4) = riktning_y(k,3)/abs(riktning_y(k,3));
+                    kundlista(j,8) = 1;% Taxibil skickas till en kund
+                    kundlista(j,11) = k;
+                    break % Avbryter loopen när en taxi tilldelas en kund.
+                else
+                    kundlista(j,8) = -1;
+                end
             end
         end
-        
-        
-        
+
         %Beräknar hur taxibilarna färdas då de ska till en kund.
         
         for k = 1:10
@@ -81,7 +102,7 @@ for i = 0:tid
                         Taxibilar(k,5) = 0;
                         kundlista(j,8) = 2;
                         kundlista(j,9) = klockan; %Sparar tiden då kunden blir upplockad
-                        disp(['Taxibil nr: ',num2str(k),' har hämtat upp kund nr: ',num2str(j)])
+                 %       disp(['Taxibil nr: ',num2str(k),' har hämtat upp kund nr: ',num2str(j)])
                     end
                 else
                     Taxibilar(k,5) = Taxibilar(k,5) + 1; % Räknar tiden för kunden att ta sig
@@ -102,7 +123,7 @@ for i = 0:tid
                         Taxibilar(k,5) = 0;
                         kundlista(j,8) = 3;
                         kundlista(j,10) = klockan; %Sparar tiden då kunden blir avlämnad
-                        disp(['Taxibil nr: ',num2str(k),' har lämnat upp kund nr: ',num2str(j)])
+                  %      disp(['Taxibil nr: ',num2str(k),' har lämnat upp kund nr: ',num2str(j)])
                     end
                 else
                     Taxibilar(k,5) = Taxibilar(k,5) + 1; % Räknar tiden för kunden att ta sig
